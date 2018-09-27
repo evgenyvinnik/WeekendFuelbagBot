0bb33b6f2dd3723e45ad3f27afa376b43b052f31
Evgeny Vinnik <evvinnik@microsoft.com>
2018-09-27 10:05:40 -0700

    Added BasicBot code.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=weekendfuelbagbot-Web-Deploy.pubxml -p:Password=qljuAbojDmmxiYSMkEYHdur9AW7Gybw6ysj1kqTyArjNDKTDPa3Ex8AjvpGJ
+
